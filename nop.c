ffef77d7ed10a1299c0b76f94aec716af84c17f5
Manuel <141195emmanuel@gmail.com>
2023-06-22 22:03:29 +0000

    second set

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+  *f_nop- empty
+  *@head: head of the stack
+  *@counter: line number
+  *Return: nothing
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
